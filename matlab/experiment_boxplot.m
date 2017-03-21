8f7fa3948b8b23449cf2122f6d35e8f4377e6d4a
Matt Barnes <barnes.matt.j@gmail.com>
2017-03-21 11:44:03 -0400

    Parameter updates

--- matlab/experiment_boxplot.m
+++ matlab/experiment_boxplot.m
@@ -1,10 +1,14 @@
 clear, clc, close all
-%load('AB_2016-12-05_15-42-47.mat');
-load('AB_2017-02-03_14-43-33.mat');
+% Synthetic experiments
+%load('AB_2017-02-03_14-43-33.mat');  % Synthetic data for known p0
+%lambda = 0.1;
+% Real experiments
+%load('AB_2017-03-11_13-21-26.mat');  % Real Adult data 2500
+load('AB_2017-03-17_07-29-51.mat');  % Real Adult data 10000
+lambda = 100;
 
 results = NaN(trials, 8);
 
-lambda = 0.1;
 for trial = 1:trials
     b = B(:, trial);
     
@@ -13,14 +17,6 @@ for trial = 1:trials
     
     %% LOCO
     x_loco = b(1);
-    
-    %% Linear regression (this does terribly)
-    %x_linear = (A'*A) \ (A' * b);
-    %x_linear = x_linear(1);
-
-    %% Ridge Regression
-    %x_ridge = (A'*A + lambda * eye(size(A, 2))) \ (A' * b);
-    %x_ridge = x_ridge(1);
     
     %% Monotonic, linear
     x_mono = trendfilter(A, b, 2, 0, true);
@@ -57,8 +53,9 @@ h_median = findobj(gcf, 'type', 'line', 'Tag', 'Median');
 set(h_median, 'Color', cmap(2,:));
 h_box = findobj('Tag','Box');
 set(h_box, 'Color', cmap(3,:));
-ylabel('Absolute error, $$|\hat x_0 - x_0|$$', 'Interpreter', 'latex')
+ylabel('Absolute error, $$|\hat e_0 - e_0|$$', 'Interpreter', 'latex')
 ylim([0 1.05*max(max(abs(results - mean(s_true(1, :)))))])
 h_xlab = xlabel('Baselines   |                                 Our method');
-set(h_xlab,'Position',get(h_xlab,'Position') - [0.8 1 0])
+%set(h_xlab,'Position',get(h_xlab,'Position') - [0.8 0 0])
+set(h_xlab,'Position',[3.7 -0.001 0])
 set(f, 'units', 'inches', 'pos', [0 0 6 4.5])
--- matlab/experiment_fullfit.m
+++ matlab/experiment_fullfit.m
@@ -1,42 +1,45 @@
 clear, clc, close all
-%load('AB_2016-12-05_13-56-12.mat')
-%load('AB_2016-12-05_15-42-47.mat')
-load('AB_2017-02-03_14-43-33.mat');
-[cmap, ~, ~] = brewermap(5, 'Set2');
+% Synthetic experiments
+%load('AB_2017-02-03_14-43-33.mat');  % Synthetic data for known p0
+%lambda = 0.1;
+% Real experiments
+%load('AB_2017-03-11_13-21-26.mat');  % Real Adult data 2500
+load('AB_2017-03-17_07-29-51.mat');  % Real Adult data 10000
+lambda = 10;
+
 
 b = mean(B, 2);
-
-lambda = 0.1;
-
+[cmap, ~, ~] = brewermap(6, 'Set2');
 f = figure; hold on
+
+%% Monotonic
+s_mono = trendfilter(A, b, 2, 0, true);
+plot((0:nT)/nT, s_mono, 'Color', cmap(1,:));
+
 %% Regularize 2nd derivative
 s_t2 = trendfilter(A, b, 2, lambda, false);
-plot((0:nT)/nT, s_t2, 'Color', cmap(1,:));
+plot((0:nT)/nT, s_t2, 'Color', cmap(2,:));
 
 %% Regularize 3rd derivative
 s_t3 = trendfilter(A, b, 3, lambda, false);
-plot((0:nT)/nT, s_t3, 'Color', cmap(2,:));
-%% Regularize 2nd derivative
+plot((0:nT)/nT, s_t3, 'Color', cmap(3,:));
+
+%% Regularize 2nd derivative, monotonic
 s_t2mono = trendfilter(A, b, 2, lambda, true);
-plot((0:nT)/nT, s_t2mono, 'Color', cmap(3,:));
-%% Regularize 3rd derivative
+plot((0:nT)/nT, s_t2mono, 'Color', cmap(4,:));
+
+%% Regularize 3rd derivative, monotonic
 s_t3mono = trendfilter(A, b, 3, lambda, true);
-plot((0:nT)/nT, s_t3mono, 'Color', cmap(4,:));
+plot((0:nT)/nT, s_t3mono, 'Color', cmap(5,:));
+
 %% Regularize 4th derivative, monotonic
 s_t4mono = trendfilter(A, b, 4, lambda, true);
-plot((0:nT)/nT, s_t4mono, 'Color', cmap(5,:));
+plot((0:nT)/nT, s_t4mono, 'Color', cmap(6,:));
 
-plot((0:nT)/nT, s_true, 'Color', 'k', 'LineStyle', '--');
+plot((0:nT)/nT, mean(s_true, 2), 'Color', 'k', 'LineStyle', '--');
 
 %% Final result of full error fit
-%f = figure;
-%plot((0:nT)/nT, s_t2,...
-%    (0:nT)/nT, s_t3,...
-%    (0:nT)/nT, s_t2mono,...
-%    (0:nT)/nT, s_t3mono,...
-%    (0:nT)/nT, s_t4mono,...
-%    (0:nT)/nT, s_true, '--')
-xlabel('Corruption')
-ylabel('MSE')
-legend('T2', 'T3', 'T2 + mono', 'T3 + mono', 'T4+mono', 'True')
+xlabel('Dependency Leakage')
+ylabel('MSE, x')
+legend('mono', 'T2', 'T3', 'T2 + mono', 'T3 + mono', 'T4+mono', 'True')
 set(f, 'units', 'inches', 'pos', [0 0 6 4.5])
