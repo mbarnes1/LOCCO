d9a05b0f876ede60c780d60f871aa3318fa792e5
Matt Barnes <barnes.matt.j@gmail.com>
2018-09-25 16:04:34 -0400

    Better variable names and output file names.

--- matlab/sample.m
+++ matlab/sample.m
@@ -1,14 +1,21 @@
 clear, clc, close all
 
 % Params
-npertrial = 10;
-trials = 5;
+dataset_name = 'synthetic';
+n_trials_per_corruption_level = 10;
+n_corruption_levels = 5;
 nprocesses = 2;
-K = npertrial*trials;  % number of bootstrap iterations per corruption level
+n_resamples_per_corruption_level = n_trials_per_corruption_level*n_corruption_levels;  % number of bootstrap iterations per corruption level
 p0 = 0.1;  % natural corruption (dist1 samples in training)
-p_clean_resample = 0.2; % Probability of resampling zero corrupted samples
-nT = floor(log(p_clean_resample)/log(1-p0));  % Number of samples in train set
 nV = 1000;  % Size of validation set
+
+% Choose training set size by specifying p_clean_resample
+%p_clean_resample = 0.002; % Probability of resampling zero corrupted samples
+%nT = floor(log(p_clean_resample)/log(1-p0));  % Number of samples in train set
+
+% Choose training set size directly
+nT = 100;
+p_clean_resample = (1-p0)^nT;
 
 % Slopes
 m0 = 1;
@@ -17,21 +24,23 @@ sigma0 = 0.5;
 sigma1 = 0.5;
 range0 = [-1, 1];
 range1 = [-10, 10];
-f = sampler_real(m0, m1, sigma0, sigma1, range0, range1);
+f = sampler(m0, m1, sigma0, sigma1, range0, range1);
 
 p = linspace(p0, 1, 2*nT);
 A = NaN(length(p), nT+1);
-B = NaN(length(p), trials);
+B = NaN(length(p), n_corruption_levels);
 
-parpool(nprocesses);
-for i = 1:length(p)
+%parpool(nprocesses);
+
+%% B3 resamples
+parfor i = 1:length(p)
     p_i = p(i);
     D = makedist('Binomial','N',nT,'p',p_i);
     A(i,:) = D.pdf(0:nT);
-    btrials = zeros(1, trials);
-    for j = 1:trials
+    btrials = zeros(1, n_corruption_levels);
+    for j = 1:n_corruption_levels
         b_jk = 0;
-        for k = 1:npertrial
+        for k = 1:n_trials_per_corruption_level
             n1 = binornd(nT, p_i);  % number of corrupted samples
             n0 = nT - n1;  % number of uncorrupted samples
             [x, y] = f.sample(n0, nT);
@@ -41,25 +50,27 @@ for i = 1:length(p)
             mse = mean((ytest-yhat).^2);
             b_jk = b_jk+mse;
         end
-        btrials(j) = b_jk/npertrial;
+        btrials(j) = b_jk/n_trials_per_corruption_level;
     end
     B(i, :) = btrials;
 end
 
-s_true = zeros(nT+1, trials);
+%% Estimate s_true
+s_true = zeros(nT+1, n_corruption_levels);
 
 parfor i = 1:length(s_true)
-    for j = 1:trials
-        for k = 1:K
+    for j = 1:n_corruption_levels
+        for k = 1:n_resamples_per_corruption_level
             [x, y] = f.sample(nT-i+1, nT);
             z = dot(x,y)/(dot(x, x));
             [xtest, ytest] = f.sample(0, nV);
             yhat = xtest*z;
             mse = mean((ytest-yhat).^2);
-            s_true(i, j) = s_true(i, j)+mse/K;
+            s_true(i, j) = s_true(i, j)+mse/n_resamples_per_corruption_level;
         end
     end
 end
 
-filename = ['AB_', datestr(now, 'yyyy-mm-dd_hh-MM-ss'), '.mat'];
-save(filename, 'trials', 'B', 'A', 'nT', 'nV', 'K', 's_true', 'npertrial', 'p0', 'm0', 'm1', 'sigma0', 'sigma1', 'range0', 'range1')
+%% Save results
+filename = ['AB_', datestr(now, 'yyyy-mm-dd_hh-MM-ss'), '_', dataset_name, '_nT', num2str(nT), '_p0', num2str(p0), '_K', num2str(n_resamples_per_corruption_level), '.mat'];
+save(filename, 'dataset_name', 'n_trials_per_corruption_level', 'n_corruption_levels', 'n_resamples_per_corruption_level', 'B', 'A', 'nT', 'nV', 's_true', 'p0', 'm0', 'm1', 'sigma0', 'sigma1', 'range0', 'range1')
